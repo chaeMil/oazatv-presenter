83aa55faa411cf8354b6cb5b1c0eb1b099f2a47a
Michal Mlejnek <chaemil72@gmail.com>
2018-09-30 16:36:11 +0200

    Add basic presentation window layout and viewmodel

--- app/server/viewmodel/presentation_viewmodel.js
+++ app/server/viewmodel/presentation_viewmodel.js
@@ -1,4 +1,5 @@
 const BaseViewModel = require('../../shared/viewmodel/base_viewmodel');
+const fabric = require('fabric').fabric;
 
 class MainViewModel extends BaseViewModel {
 
@@ -8,6 +9,30 @@ class MainViewModel extends BaseViewModel {
 
     init() {
         super.init();
+        this._initCanvas();
+    }
+
+    _initCanvas() {
+        this.canvasWrapper = document.querySelector('#canvas-wrapper');
+        this.canvas = new fabric.Canvas('canvas', {
+            selection: true,
+            uniScaleTransform: true,
+            backgroundColor: '#FFFFFF'
+        });
+
+        window.addEventListener('resize', () => {
+            this._resizeCanvas();
+        }, false);
+        this._resizeCanvas();
+    }
+
+    _resizeCanvas() {
+        let padding = 2 * 25;
+        this.canvas.setWidth(this.canvasWrapper.offsetWidth - padding);
+        this.canvas.setHeight((this.canvasWrapper.offsetWidth - padding) / (16 / 9));
+        let scale = (this.canvasWrapper.offsetWidth - padding) / 1280;
+        this.canvas.setZoom(scale);
+        this.canvas.renderAll();
     }
 
     savePresentation() {
@@ -16,6 +41,10 @@ class MainViewModel extends BaseViewModel {
 
     loadPresentation() {
         //TODO
+    }
+
+    showAddSlideMenu() {
+        document.querySelector("#add-slide-menu").classList.remove("hidden");
     }
 }
 
